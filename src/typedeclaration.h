83afbdefb08d7f8c43e09a198cb6718a4f6e8305
memeda <readonlyfile@hotmail.com>
2016-04-24 19:21:34 +0800

    fix push BUG ;

--- src/typedeclaration.h
+++
@@ -1,8 +0,0 @@
-#ifndef TYPEDECLARATION_INCLUDED_H
-#define TYPEDECLARATION_INCLUDED_H
-namespace slnn{
-    using Index = int; // cnn::Dict return `int` as index 
-    using IndexSeq = vector<Index>;
-    using InstancePair = pair<IndexSeq, IndexSeq>;
-}
-#endif
