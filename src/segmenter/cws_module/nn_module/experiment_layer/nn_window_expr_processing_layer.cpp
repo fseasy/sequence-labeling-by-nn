20d3443bdcc94ab808856a08e79e5e51268da302
memeda <readonlyfile@hotmail.com>
2016-10-25 22:21:13 +0800

    doing attention(30%).

--- src/segmenter/cws_module/nn_module/experiment_layer/nn_window_expr_processing_layer.cpp
+++ src/segmenter/cws_module/nn_module/experiment_layer/nn_window_expr_processing_layer.cpp
@@ -31,7 +31,7 @@ WindowExprBigramLayer::WindowExprBigramLayer(dynet::Model *dynet_model, unsigned
     cached_c_expr_list(window_sz - 1),
     output_dim(unit_embedding_dim)
 {
-    assert(window_sz > 0);
+    assert(window_sz > 1);
 }
 
 
@@ -71,6 +71,95 @@ WindowExprBigramLayer::process(const std::vector<std::vector<dynet::expr::Expres
         bigram_expr_list[j] = dynet::expr::sum(cached_c_expr_list);
     }
     return bigram_expr_list;
+}
+
+/***********
+ * Window Expression Bigram by Concat Layer
+ **********/
+
+WindowExprBigramConcatLayer::WindowExprBigramConcatLayer(dynet::Model *dynet_model, unsigned unit_embedding_dim, unsigned window_sz)
+    :Tl_param(dynet_model->add_parameters({ unit_embedding_dim, unit_embedding_dim })),
+    Tr_param(dynet_model->add_parameters({ unit_embedding_dim, unit_embedding_dim })),
+    b_param(dynet_model->add_parameters({ unit_embedding_dim })),
+    cached_c_expr_list(window_sz - 1),
+    output_dim(unit_embedding_dim * (window_sz - 1))
+{
+    assert(window_sz > 1);
+}
+
+std::vector<dynet::expr::Expression>
+WindowExprBigramConcatLayer::process(const std::vector<std::vector<dynet::expr::Expression>> &window_expr_list)
+{
+    unsigned len = window_expr_list.size();
+    std::vector<dynet::expr::Expression> bigram_expr_list(len);
+    // the first cached c expression
+    for( unsigned i = 0; i < cached_c_expr_list.size(); ++i )
+    {
+        cached_c_expr_list[i] = dynet::expr::tanh(
+            dynet::expr::affine_transform({
+            b_expr,
+            Tl_expr, window_expr_list[0][i],
+            Tr_expr, window_expr_list[0][i + 1]
+        })
+        );
+    }
+    bigram_expr_list[0] = dynet::expr::concatenate(cached_c_expr_list);
+    // the continues expression
+    // -> rolling
+    // bigram: current: [AB, BC, CD, DE,] EF, FG, ...
+    //         next   : AB, [BC, CD, DE, EF,] EG, ...
+    // input window: current: [A,B,C,D,E],[B,C,D,E,F]
+    for( unsigned j = 1; j < len; ++j )
+    {
+        cached_c_expr_list.pop_front();
+        dynet::expr::Expression next_expr = dynet::expr::tanh(
+            dynet::expr::affine_transform({
+            b_expr,
+            Tl_expr, *(window_expr_list[j].end() - 2),
+            Tr_expr, window_expr_list[j].back()
+        })
+        );
+        cached_c_expr_list.push_back(next_expr);
+        bigram_expr_list[j] = dynet::expr::concatenate(cached_c_expr_list);
+    }
+    return bigram_expr_list;
+}
+
+/**********
+ * Window Expr Attention 1 Layer
+ **********/
+WindowExprAttention1Layer::WindowExprAttention1Layer(dynet::Model *dynet_model,
+    unsigned unit_embedding_dim, unsigned window_sz)
+    : W(dynet_model->add_parameters({ unit_embedding_dim, unit_embedding_dim })),
+    U(dynet_model->add_parameters({ unit_embedding_dim, unit_embedding_dim })),
+    v(dynet_model->add_parameters({ 1, unit_embedding_dim })),
+    output_dim(unit_embedding_dim * window_sz),
+    window_sz(window_sz)
+{}
+
+
+std::vector<dynet::expr::Expression>
+WindowExprAttention1Layer::process(const std::vector<std::vector<dynet::expr::Expression>>& window_expr_list)
+{
+    // init first 
+    unsigned half_sz = window_sz / 2;
+    std::deque<dynet::expr::Expression> left_context_mul(half_sz);
+    std::deque<dynet::expr::Expression> right_context_mul(half_sz);
+    for( unsigned i = 0; i < half_sz; ++i )
+    {
+        left_context_mul[i] = W_expr * window_expr_list[0][i];
+        right_context_mul[i] = W_expr * window_expr_list[0][half_sz + i + 1];
+    }
+    dynet::expr::Expression center_mul = U_expr * window_expr_list[0][half_sz];
+    std::vector<dynet::expr::Expression> score_list(window_sz - 1);
+    for( unsigned i = 0; i < half_sz; ++i )
+    {
+        score_list[i] = v_expr * dynet::expr::tanh(left_context_mul[i] + center_mul);
+        score_list[i + half_sz] = v_expr * dynet::expr::tanh(right_context_mul[i] + center_mul);
+    }
+    dynet::expr::Expression weight_list = dynet::expr::softmax(dynet::expr::concatenate(score_list));
+    std::vector<dynet::expr::Expression> result_expr_window(window_sz);
+
 }
 
 /***********
--- src/segmenter/cws_module/nn_module/experiment_layer/nn_window_expr_processing_layer.h
+++ src/segmenter/cws_module/nn_module/experiment_layer/nn_window_expr_processing_layer.h
@@ -64,7 +64,6 @@ public:
     std::vector<dynet::expr::Expression>
         process(const std::vector<std::vector<dynet::expr::Expression>>& window_expr_list) override;
 private:
-    dynet::ComputationGraph *pcg;
     dynet::Parameter Tl_param;
     dynet::Parameter Tr_param;
     dynet::Parameter b_param;
@@ -73,6 +72,53 @@ private:
     dynet::expr::Expression b_expr;
     std::deque<dynet::expr::Expression> cached_c_expr_list;
     unsigned output_dim;
+};
+
+
+class WindowExprBigramConcatLayer : public WindowExprProcessingLayerAbstract
+{
+    // variant to bigram model
+public:
+    WindowExprBigramConcatLayer(dynet::Model *dynet_model, unsigned unit_embedding_dim, unsigned window_sz);
+    WindowExprBigramConcatLayer(const WindowExprBigramConcatLayer&) = delete;
+    WindowExprBigramConcatLayer& operator=(const WindowExprBigramConcatLayer&) = delete;
+public:
+    void new_graph(dynet::ComputationGraph &rcg) override;
+    unsigned get_output_dim() const override{ return output_dim; }
+    std::vector<dynet::expr::Expression>
+        process(const std::vector<std::vector<dynet::expr::Expression>>& window_expr_list) override;
+private:
+    dynet::Parameter Tl_param;
+    dynet::Parameter Tr_param;
+    dynet::Parameter b_param;
+    dynet::expr::Expression Tl_expr;
+    dynet::expr::Expression Tr_expr;
+    dynet::expr::Expression b_expr;
+    std::deque<dynet::expr::Expression> cached_c_expr_list;
+    unsigned output_dim;
+};
+
+class WindowExprAttention1Layer : public WindowExprProcessingLayerAbstract
+{
+public:
+    WindowExprAttention1Layer(dynet::Model *dynet_model, unsigned unit_embedding_dim, unsigned window_sz);
+    WindowExprAttention1Layer(const WindowExprAttention1Layer&) = delete;
+    WindowExprAttention1Layer& operator=(const WindowExprAttention1Layer&) = delete;
+public:
+    void new_graph(dynet::ComputationGraph &rcg) override;
+    unsigned get_output_dim() const override{ return output_dim; }
+    std::vector<dynet::expr::Expression>
+        process(const std::vector<std::vector<dynet::expr::Expression>>& window_expr_list) override;
+private:
+    dynet::Parameter W;
+    dynet::Parameter U;
+    dynet::Parameter v;
+    dynet::expr::Expression W_expr;
+    dynet::expr::Expression U_expr;
+    dynet::expr::Expression v_expr;
+    dynet::ComputationGraph *pcg;
+    unsigned output_dim;
+    unsigned window_sz;
 };
 
 
@@ -127,12 +173,34 @@ WindowExprSumLayer::process(const std::vector<std::vector<dynet::expr::Expressio
 inline
 void WindowExprBigramLayer::new_graph(dynet::ComputationGraph &rcg)
 {
-    pcg = &rcg;
     Tl_expr = dynet::expr::parameter(rcg, Tl_param);
     Tr_expr = dynet::expr::parameter(rcg, Tr_param);
     b_expr = dynet::expr::parameter(rcg, b_param);
 }
 
+/**************
+ * Window Expression Bigram by Concat Layer
+ **************/
+
+inline
+void WindowExprBigramConcatLayer::new_graph(dynet::ComputationGraph &rcg)
+{
+    Tl_expr = dynet::expr::parameter(rcg, Tl_param);
+    Tr_expr = dynet::expr::parameter(rcg, Tr_param);
+    b_expr = dynet::expr::parameter(rcg, b_param);
+}
+
+/************
+ * Window Expression Attention 1 Layer
+ ************/
+inline
+void WindowExprAttention1Layer::new_graph(dynet::ComputationGraph &rcg)
+{
+    pcg = &rcg;
+    W_expr = dynet::expr::parameter(rcg, W);
+    U_expr = dynet::expr::parameter(rcg, U);
+    v_expr = dynet::expr::parameter(rcg, v);
+}
 
 
 } // end of namespace experiment
