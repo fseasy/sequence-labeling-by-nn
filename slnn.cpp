64fb62beaa03fa9cb4025a4f5e09d0750694317c
memeda <readOnlyFile@hotmail.com>
2017-05-24 23:45:59 +0800

    init cipying!

---
+++ slnn.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "start my graduation coding!\n"
+              << "I'll start by coping dynet!\n";
+    
+    return 0;
+}
